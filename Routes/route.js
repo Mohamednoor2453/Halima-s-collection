a2de5fa36788134d23c7bb9f85aec1e57eb91889
Mohamednoor2453 <mohamednoornoor10@gmail.com>
2024-10-14 10:39:15 +0300

    added route for admin and login in route.js file

--- Routes/route.js
+++ Routes/route.js
@@ -5,11 +5,21 @@ const path = require('path')
 const router = express.Router()
 router.use(express.static('public'))
 
+//sending home page to user
 router.get('/', (req, res)=>{
     res.sendFile(path.join(__dirname, '../public/home.html'));
 })
 
+//sending admin page to user
 router.get('/admin', (req, res)=>{
     res.sendFile(path.join(__dirname, '../public/admin.html'))
 })
+
+router.get('/Login', (req, res)=>{
+    res.sendFile(path.join(__dirname, '../public/login.html'))
+})
+
+router.get('/SignUp', (req, res)=>{
+    res.sendFile(path.join(__dirname,  '../public/signup.html'))
+} )
 module.exports = router
