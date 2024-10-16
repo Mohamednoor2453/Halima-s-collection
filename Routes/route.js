6917b435cdf040fc33971648e0d67f3bb50ace13
Hp <hafsanoor401@gmail.com>
2024-10-16 12:14:06 +0300

    new file all products

--- Routes/route.js
+++ Routes/route.js
@@ -20,5 +20,8 @@ router.get('/admin', (req, res) => {
 router.get('/addingProducts', (req, res) => {
     res.sendFile(path.join(__dirname, '../public/addproducts.html'));
 });
+router.get('/addedProducts', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/allproducts.html'));
+});
 
 module.exports = router;
