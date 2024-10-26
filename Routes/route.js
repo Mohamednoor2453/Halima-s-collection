9130a2546478111a0f2c9d44694c1b75c7ca29e2
Mohamednoor2453 <mohamednoornoor10@gmail.com>
2024-10-26 10:29:48 +0300

    updated route.ejs

--- Routes/route.js
+++ Routes/route.js
@@ -1,5 +1,3 @@
-// route.js
-
 const express = require('express');
 const path = require('path');
 
@@ -20,9 +18,10 @@ router.get('/admin', (req, res) => {
 router.get('/addingProducts', (req, res) => {
     res.sendFile(path.join(__dirname, '../public/addproducts.html'));
 });
+
+// Sending all products page to user
 router.get('/addedProducts', (req, res) => {
     res.sendFile(path.join(__dirname, '../public/allproducts.html'));
 });
-
 
 module.exports = router;
